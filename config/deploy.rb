013f3b2ed7129de22327fe0c0fca96b112ef48b7
SAITO, Takashi <t-saito@esm.co.jp>
2017-03-15 18:50:43 +0900

    for sample

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.7.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'hello_app'
+set :repo_url, 'git@github.com:t-saito/hello_app.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
